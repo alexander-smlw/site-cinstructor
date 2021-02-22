88f68a09636c2d0f088faa94198dc2abad8dc84c
Aleksander <smlwmy@gmail.com>
2021-02-22 12:42:27 +0500

    init & write code base

---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+  { type: 'title', value: 'Hello world from JS!'},
+  { type: 'text', value: 'here we go with some text!'},
+  { type: 'columns', value: [
+      '1111111111',
+      '2222222222',
+      '3333333333',
+      '4444444444'
+    ]
+  },
+  { type: 'image', value: '../assets/image.jpg'}
+]
