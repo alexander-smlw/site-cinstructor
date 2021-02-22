f95726ae8a6c977b32b1fbe4d5ea52f541bddddf
Aleksander <smlwmy@gmail.com>
2021-02-22 14:02:44 +0500

    move generate block logic to class. Delete file `templates`

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import { model } from './model.js'
-import { templates } from './templates.js'
 import './styles/main.css'
 
 const $site = document.querySelector('#site')
 
 model.forEach(block => {
-  const toHTML = templates[block.type]
-
-  if (toHTML) {
-    $site.insertAdjacentHTML('beforeend', toHTML(block))
-  }
-
+  $site.insertAdjacentHTML('beforeend', block.toHTML())
 })
 
--- src/model.js
+++ src/model.js
@@ -1,63 +1,49 @@
-import { green } from 'colorette'
-import M from 'minimatch'
 import image from './assets/image.jpg'
+import { TitleBlock, ImageBlock, ColumnsBlock, TextBlock  } from './classes/blocks'
 
 export const model = [
-  {
-    type: 'title',
-    value: 'Конструктор сайтов на чистом JavaScript',
-    options: {
-      tag: 'h2',
-      styles: {
-        background: 'red',
-        color: '#fff',
-        padding: '1.5rem',
-        'text-align': 'center'
-      }
+  new TitleBlock('Конструктор сайтов на чистом JavaScript', {
+    tag: 'h2',
+    styles: {
+      background: 'red',
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center'
     }
-  },
-  {
-    type: 'text',
-    value: 'here we go with some text!',
-    options: {
-      styles: {
-        background: 'yellow',
-        padding: '1rem',
-        'font-weight': 'bold'
-      }
+  }),
+
+  new TextBlock('here we go with some text!', {
+    styles: {
+      background: 'yellow',
+      padding: '1rem',
+      'font-weight': 'bold'
     }
-  },
-  {
-    type: 'columns',
-    value: [
-      '1111111111',
-      '2222222222',
-      '3333333333',
-      '4444444444'
-    ],
-    options: {
-      styles: {
-        background: 'green',
-        padding: '1rem',
-        color: '#fff',
-        'font-weight': 'bold'
-      }
+  }),
+
+  new ColumnsBlock([
+    '1111111111',
+    '2222222222',
+    '3333333333',
+    '4444444444'
+  ], {
+    styles: {
+      background: 'green',
+      padding: '1rem',
+      color: '#fff',
+      'font-weight': 'bold'
     }
-  },
-  {
-    type: 'image',
-    value: image,
-    options: {
-      styles: {
-        padding: '2rem 0',
-        display: 'flex',
-        'justify-content': 'center'
-      },
-      imageStyles: {
-        width: '500px',
-        height: 'auto'
-      },
-      alt: 'Это картинка'
+  }),
+
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
     },
-  }
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'Это картинка'
+  })
 ]
