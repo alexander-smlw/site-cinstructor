874069f690f7364e4eedc089ef69fca0290eb466
Aleksander <smlwmy@gmail.com>
2021-02-22 13:41:26 +0500

    add `css` function for style from options

--- src/model.js
+++ src/model.js
@@ -1,14 +1,63 @@
+import { green } from 'colorette'
+import M from 'minimatch'
 import image from './assets/image.jpg'
 
 export const model = [
-  { type: 'title', value: 'Hello world from JS!'},
-  { type: 'text', value: 'here we go with some text!'},
-  { type: 'columns', value: [
+  {
+    type: 'title',
+    value: 'Конструктор сайтов на чистом JavaScript',
+    options: {
+      tag: 'h2',
+      styles: {
+        background: 'red',
+        color: '#fff',
+        padding: '1.5rem',
+        'text-align': 'center'
+      }
+    }
+  },
+  {
+    type: 'text',
+    value: 'here we go with some text!',
+    options: {
+      styles: {
+        background: 'yellow',
+        padding: '1rem',
+        'font-weight': 'bold'
+      }
+    }
+  },
+  {
+    type: 'columns',
+    value: [
       '1111111111',
       '2222222222',
       '3333333333',
       '4444444444'
-    ]
+    ],
+    options: {
+      styles: {
+        background: 'green',
+        padding: '1rem',
+        color: '#fff',
+        'font-weight': 'bold'
+      }
+    }
   },
-  { type: 'image', value: image}
+  {
+    type: 'image',
+    value: image,
+    options: {
+      styles: {
+        padding: '2rem 0',
+        display: 'flex',
+        'justify-content': 'center'
+      },
+      imageStyles: {
+        width: '500px',
+        height: 'auto'
+      },
+      alt: 'Это картинка'
+    },
+  }
 ]
--- src/templates.js
+++ src/templates.js
@@ -1,21 +1,31 @@
-import { row, col } from './utils'
+import { row, col, css } from './utils'
 
 function title(block) {
-  return row(col(`<h1>${block.value}</h1>`))
+  const { tag = 'h1', styles } = block.options
+
+  return row(col(`<${tag}>${block.value}</${tag}>`), css(styles))
 }
 
 function text(block) {
-  return row(col(`<p>${block.value}</p>`))
+  const { styles } = block.options
+
+  return row(col(`<p>${block.value}</p>`), css(styles))
 }
 
 function columns(block) {
+  const { styles } = block.options
+
   const html = block.value.map(col).join('')
 
-  return row(html)
+  return row(html, css(styles))
 }
 
 function image(block) {
-  return row(`<img src="${block.value}" />`)
+  const { imageStyles: is, alt = '', styles } = block.options
+
+  console.log(imageStyles, block.options)
+
+  return row(`<img alt="${alt}" src="${block.value}" style="${css(is)}" />`, css(styles))
 }
 
 export const templates = {
